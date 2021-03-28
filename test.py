536047df5e97f56736a28382705be6835135e533
chloekim917 <chloekim917@gmail.com>
2021-03-28 15:55:47 -0400

    test1

--- test.py
+++ test.py
@@ -31,8 +31,8 @@ url = 'https://www.reddit.com/r/DunderMifflin/comments/mf1y4j/makes_it_all_worth
 
 driver.get(url)
 
-html =  BeautifulSoup(driver.page_source, "html.parser")
+# html =  BeautifulSoup(driver.page_source, "html.parser")
 
-result = html.find_all("p",{"class":"_1qeIAgB0cPwnLhDF9XSiJM"})
-for item in result:
-    print(item.text)
+# result = html.find_all("p",{"class":"_1qeIAgB0cPwnLhDF9XSiJM"})
+# for item in result:
+#     print(item.text)
