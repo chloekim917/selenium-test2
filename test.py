92dbb494b0de5a8a0a7a3beab616846624f66973
chloekim917 <chloekim917@gmail.com>
2021-03-28 16:03:23 -0400

    done

--- test.py
+++ test.py
@@ -1,27 +1,3 @@
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-
-# driver = webdriver.Chrome()
-
-# url = 'https://www.youtube.com/watch?v=7UKtY3zp4sA&ab_channel=BjornSteinbekk-Aguywithadrone'
-
-# driver.get(url)
-
-# # print(driver.page_source)
-
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'style-scope ytd-comment-renderer')))
-
-
-# html =  BeautifulSoup(driver.page_source, "html.parser")
-
-# result = html.find_all("yt-formatted-string",{"class":"style-scope ytd-comment-renderer"})
-# for item in result:
-#     print(item.text)
-
-
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -31,8 +7,9 @@ url = 'https://www.reddit.com/r/DunderMifflin/comments/mf1y4j/makes_it_all_worth
 
 driver.get(url)
 
-# html =  BeautifulSoup(driver.page_source, "html.parser")
+html =  BeautifulSoup(driver.page_source, "html.parser")
 
-# result = html.find_all("p",{"class":"_1qeIAgB0cPwnLhDF9XSiJM"})
-# for item in result:
-#     print(item.text)
+result = html.find_all("p",{"class":"_1qeIAgB0cPwnLhDF9XSiJM"})
+
+for item in result:
+    print(item.text)
