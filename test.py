bc07bebb283977a40906c32720806603432bd9fe
chloekim917 <chloekim917@gmail.com>
2021-03-28 15:25:39 -0400

    test successful

--- test.py
+++ test.py
@@ -1,21 +1,38 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome('chromedriver.exe')
+# driver = webdriver.Chrome()
 
-url = 'https://www.youtube.com/watch?v=7UKtY3zp4sA&ab_channel=BjornSteinbekk-Aguywithadrone'
+# url = 'https://www.youtube.com/watch?v=7UKtY3zp4sA&ab_channel=BjornSteinbekk-Aguywithadrone'
+
+# driver.get(url)
+
+# # print(driver.page_source)
+
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'style-scope ytd-comment-renderer')))
+
+
+# html =  BeautifulSoup(driver.page_source, "html.parser")
+
+# result = html.find_all("yt-formatted-string",{"class":"style-scope ytd-comment-renderer"})
+# for item in result:
+#     print(item.text)
+
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+url = 'https://www.reddit.com/r/DunderMifflin/comments/mf1y4j/makes_it_all_worth_it/'
 
 driver.get(url)
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'u_cbox_area')))
-
 html =  BeautifulSoup(driver.page_source, "html.parser")
 
-result = html.find_all("div",{"class":"style-scope ytd-comment-renderer"})
+result = html.find_all("p",{"class":"_1qeIAgB0cPwnLhDF9XSiJM"})
 for item in result:
     print(item.text)
-
-
