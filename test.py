447b29d7fc28fd49f96c28058e0e6d3c1d10d55a
chloekim917 <chloekim917@gmail.com>
2021-03-28 14:25:30 -0400

    setup

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('chromedriver.exe')
 
-url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=105&oid=421&aid=0004932341'
+url = 'https://www.youtube.com/watch?v=7UKtY3zp4sA&ab_channel=BjornSteinbekk-Aguywithadrone'
 
 driver.get(url)
 
@@ -14,7 +14,7 @@ WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,'u
 
 html =  BeautifulSoup(driver.page_source, "html.parser")
 
-result = html.find_all("span",{"class":"u_cbox_contents"})
+result = html.find_all("div",{"class":"style-scope ytd-comment-renderer"})
 for item in result:
     print(item.text)
 
